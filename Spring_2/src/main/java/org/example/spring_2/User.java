1408b2287b5a532bb804dd6c969e1c0dd2c87c48
darzen <nivnik004@gmail.com>
2024-12-12 20:16:03 +0100

    Added AuthService. Changes in User, UserRepository.

--- Spring_2/src/main/java/org/example/spring_2/User.java
+++ Spring_2/src/main/java/org/example/spring_2/User.java
@@ -1,6 +1,8 @@
 package org.example.spring_2;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Entity
@@ -15,13 +17,22 @@ public class User {
 
     @Id
     @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     @Column(name = "username", unique = true, nullable = false)
-    private String name;
+    @NotNull
+    @NotEmpty
+    private String username;
+
     @Column(name = "password", nullable = false)
+    @NotNull
+    @NotEmpty
     private String password;
+
     @Column(name = "permission_level", nullable = false)
+    @NotNull
+    @NotEmpty
     private int permission_level;
 
 }
--- Spring_2/src/main/java/org/example/spring_2/UserRepository.java
+++ Spring_2/src/main/java/org/example/spring_2/UserRepository.java
@@ -1,10 +1,9 @@
 package org.example.spring_2;
 
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Integer> {
-
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByUsername(String username);
 }
