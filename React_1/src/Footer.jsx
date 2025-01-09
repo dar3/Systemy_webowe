56244638b471fee39d1f5b66ac03cf14d2bd1ab1
Darko <darkoshypka@gmail.com>
2025-01-09 21:25:05 +0100

    First React exercise done

---
+++ React_1/src/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+    <footer>
+      <p>
+        {" "}
+        &copy; {new Date().getFullYear()} The best website that was ever made{" "}
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
